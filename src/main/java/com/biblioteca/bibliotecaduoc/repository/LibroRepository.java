c36417b6da05570d2c824392ded1166535630e51
Patricio <patriciojimenez838@gmail.com>
2025-05-04 05:26:22 -0400

    Se crean metodos de guardar libros en arreglo y actualizar libros

--- src/main/java/com/biblioteca/bibliotecaduoc/repository/LibroRepository.java
+++ src/main/java/com/biblioteca/bibliotecaduoc/repository/LibroRepository.java
@@ -37,4 +37,34 @@ public class LibroRepository {
         }
         return null;
     }
+
+    //Guarda libro en arreglo
+    public Libro guardar(Libro lib) {
+        listaLibros.add(lib);
+        return lib;
+    }
+
+    //
+    public Libro actualizar(Libro lib) {
+        int id = 0;
+        int idPosicion = 0;
+
+        for (int i = 0; i < listaLibros.size(); i++) {
+            if (listaLibros.get(i).getId() == lib.getId()){
+                id = lib.getId();
+                idPosicion = i;
+            }
+        }
+
+        Libro libro1 = new Libro();
+        libro1.setId(id);
+        libro1.setTitulo(lib.getTitulo());
+        libro1.setAutor(lib.getAutor());
+        libro1.setFechaPublicacion(lib.getFechaPublicacion());
+        libro1.setEditorial(lib.getEditorial());
+        libro1.setIsbn(lib.getIsbn());
+
+        listaLibros.add(idPosicion, libro1);
+        return libro1;
+    }
 }
