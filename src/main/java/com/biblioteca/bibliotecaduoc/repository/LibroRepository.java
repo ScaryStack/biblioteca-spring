f0f63eff0b503b5b45dbe7c8912db92abdc640ff
Patricio <patriciojimenez838@gmail.com>
2025-05-04 05:04:34 -0400

    Se crea arreglo de libros y un metodo que retorna una lista de libros

--- src/main/java/com/biblioteca/bibliotecaduoc/repository/LibroRepository.java
+++ src/main/java/com/biblioteca/bibliotecaduoc/repository/LibroRepository.java
@@ -1,4 +1,21 @@
 package com.biblioteca.bibliotecaduoc.repository;
 
+
+import com.biblioteca.bibliotecaduoc.model.Libro;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+
 public class LibroRepository {
+
+    //Arreglo que guarda libros
+    private List<Libro> listaLibros = new ArrayList<>();
+
+    //Metodo que retorna lista de libros
+    public List<Libro> obtenerLibros() {
+        return listaLibros;
+    }
 }
