4eac889831f2a15c733e3866c3426889e4acff57
Patricio <patriciojimenez838@gmail.com>
2025-05-04 05:19:49 -0400

    Se crean metodos de busquedas de libros por id y por isbn

--- src/main/java/com/biblioteca/bibliotecaduoc/repository/LibroRepository.java
+++ src/main/java/com/biblioteca/bibliotecaduoc/repository/LibroRepository.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Repository
-
 public class LibroRepository {
 
     //Arreglo que guarda libros
@@ -17,5 +16,25 @@ public class LibroRepository {
     //Metodo que retorna lista de libros
     public List<Libro> obtenerLibros() {
         return listaLibros;
+    }
+
+    //Busqueda de libro por id
+    public Libro buscarPorId(int id) {
+        for (Libro libro : listaLibros) {
+            if (libro.getId() == id){
+                return libro;
+            }
+        }
+        return null;
+    }
+
+    //Busqueda de libro por isbn
+    public Libro buscarPorIsbn(String isbn) {
+        for (Libro libro : listaLibros) {
+            if (libro.getIsbn().equals(isbn)){
+                return libro;
+            }
+        }
+        return null;
     }
 }
